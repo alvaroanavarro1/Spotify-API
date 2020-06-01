83638e3ad146308e0887e88b8cd2778a7c7be94a
alvaroanavarro1 <38303261+alvaroanavarro1@users.noreply.github.com>
2020-06-01 18:28:18 -0400

    First aproach of the webapp design. Enviroment and some flask code

---
+++ app/__init__.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+# to activate the enviroment
+#(base) C:\Users\alvar\Documents\GitHub\Spotify-API>APPUI\Scripts\activate.bat
+
+app = Flask(__name__)
+
+from app import views
+from app import admin_views
+
+
